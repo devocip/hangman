b74536c7054294c0a0735cc1b63c132d50684761
devocip <92547706+devocip@users.noreply.github.com>
2022-01-08 11:50:02 +0300

    Delete my_page directory

--- my_page/week_days/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from week_days import views
-
-urlpatterns = [
-    path('<int:day>', views.get_day_by_number),
-    path('<str:day>', views.get_day, name='name_day'),
-    ]
-
